5894bd77a3ad8b7c5e7d816994aa5c26074ad124
maryameB <boulhajat.maryame@gmail.com>
2018-06-10 03:23:25 +0200

    Add(ORM): create alembic env

---
+++ almebic/versions/6c9cbbaf2d79_create_first_revision.py
@@ -0,0 +1,22 @@
+"""create first revision
+
+Revision ID: 6c9cbbaf2d79
+Revises: 
+Create Date: 2018-06-10 03:21:02.480677
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '6c9cbbaf2d79'
+down_revision =  None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
