9fd944ba70a017a3e9c43289ca2774531e89b303
maryameB <boulhajat.maryame@gmail.com>
2018-06-10 19:20:58 +0200

    Add(Requrements): add isort and ipdb libs

--- almebic/versions/6c9cbbaf2d79_create_first_revision.py
+++ almebic/versions/6c9cbbaf2d79_create_first_revision.py
@@ -1,17 +1,14 @@
 """create first revision
 
 Revision ID: 6c9cbbaf2d79
-Revises: 
+Revises:
 Create Date: 2018-06-10 03:21:02.480677
-
 """
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
+
 revision = '6c9cbbaf2d79'
-down_revision =  None
+down_revision = None
 
 
 def upgrade():
